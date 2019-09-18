4982c7233deb5bf37a7e5db13c59179be15203c9
Sara M <saram@lagash.com>
2019-09-18 15:53:49 -0500

    3.5reverseInParentheses (DONE!)

--- 3.5reverseInParentheses.js
+++ 3.5reverseInParentheses.js
@@ -157,45 +157,145 @@ console.log(newT);
 
 
 //Funcionan 5/7 (fallan los que tienen () () separados )
+/*
 function reverseInParentheses(text) {
     if (text === 'foo(bar)baz(blim)' || text === '(abc)d(efg)'){
       return 'foo(bar)baz(blim)';
     }
-    //console.log(text);
-  //console.log(text.length);
+    console.log(text);
+  console.log(text.length);
     let newT = text;
     let ultimoParentesis = newT.indexOf("(");
-    //console.log('indexOF' + newT.indexOf("("));
+    console.log('indexOF' + newT.indexOf("("));
   while (ultimoParentesis !== -1){
     var start = newT.lastIndexOf("(");
-    //console.log(start);
+    console.log(start);
     
     var end = newT.indexOf(")");
-    //console.log(end); 
+    console.log(end); 
     
     let spaceChange = (end - start) + 1;
-    //console.log(spaceChange);
+    console.log(spaceChange);
     
-    //console.log("from " + start + " - to " + end);
+    console.log("from " + start + " - to " + end);
     
     var subtext = newT.substring(start + 1, end);
-    //console.log(subtext);
+    console.log(subtext);
     let newSubText = subtext.split("");
-    //console.log(newSubText);
+    console.log(newSubText);
     let addText = newSubText.reverse().join("");
-    //console.log(addText);
+    console.log(addText);
     console.log(newT.substring(start, end + 1))
     
     newT = newT.replace(newT.substring(start, end + 1),addText);
     console.log(newT);
+*/
+
+    // reverseInParentheses 2
+//
+//
+
+// let inputString = "foo(bar)baz(blim)rger";
+
+// let totalLetters = inputString.length;
+// console.log("Total letters " + totalLetters);
+
+// let start = inputString.lastIndexOf("(");
+// console.log("Start in " + start);
+
+// // let piece = (totalLetters - start) + 1;
+// // console.log("Piece " + piece);
+
+// // console.log("from "+ start + " take " + piece);
+
+// let choose = inputString.substring(start, totalLetters);
+// console.log(choose);
+
+// let  fragment = choose.indexOf(")");
+// console.log(fragment);
+
+// let section = choose.substring(1, fragment);
+// console.log(section);
+
+
   
-    ultimoParentesis = newT.indexOf("(");
-  }
-    return callData(newT);
-  }
+//     ultimoParentesis = newT.indexOf("(");
+//   }
+//     return callData(newT);
+//   }
   
   
-  function callData(newT1){
-    return newT1;
+//   function callData(newT1){
+//     return newT1;
+//   }
+
+// reverseInParentheses 3
+//
+//
+/*
+let inputString = "foo(bar)baz(blim)rger";
+
+let totalLetters = inputString.length;
+console.log("Total letters " + totalLetters);
+
+let start = inputString.lastIndexOf("(");
+console.log("Start in " + start);
+
+let choose = inputString.substring(start, totalLetters);
+console.log(choose);
+let ultimoParentesis = inputString.lastIndexOf("(");
+while (ultimoParentesis != -1){
+  let  fragment = choose.indexOf(")");
+    console.log(fragment);
+  var replaced = choose.substring(0, fragment + 1);
+  let section = choose.substring(1, fragment);
+    console.log(section);
+  let splitSection = section.split("");
+    console.log(splitSection);
+  let newSelection = splitSection.reverse().join("");
+    console.log(newSelection);
+  let newText = inputString.replace(replaced, newSelection);
+    console.log(newText);
+
+    ultimoParentesis = newText.lastIndexOf("(");
+    console.log(ultimoParentesis);
+    inputString = newText;
+}
+*/
+
+//FINAL -funciona FULL
+
+function reverseInParentheses(text) {
+    let inputString = text;
+    let ultimoParentesis = inputString.lastIndexOf("(");
+    while (ultimoParentesis != -1){
+    
+      let totalLetters = inputString.length;
+      console.log("Total letters " + totalLetters);
+  
+      let start = inputString.lastIndexOf("(");
+      console.log("Start in " + start);
+  
+      let choose = inputString.substring(start, totalLetters);
+      console.log(choose);
+      
+    
+      let  fragment = choose.indexOf(")");
+        console.log(fragment);
+      var replaced = choose.substring(0, fragment + 1);
+      let section = choose.substring(1, fragment);
+        console.log(section);
+      let splitSection = section.split("");
+        console.log(splitSection);
+      let newSelection = splitSection.reverse().join("");
+        console.log(newSelection);
+      let newText = inputString.replace(replaced, newSelection);
+        console.log(newText);
+  
+        ultimoParentesis = newText.lastIndexOf("(");
+        console.log(ultimoParentesis);
+        inputString = newText;
+    }
+    return inputString;
   }
 
